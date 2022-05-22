ff12215de92148943aaa28dbd71fddd4347980ae
gtbrady <gtbrady89@gmail.com>
2022-05-22 17:31:36 -0500

    1.2.4 Adding DAOs

---
+++ src/DAO/DBClient.java
@@ -0,0 +1,126 @@
+package DAO;
+
+import Model.Client;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class DBClient {
+
+    public static ObservableList<Client> getClients() {
+        ObservableList<Client> clientList = FXCollections.observableArrayList();
+        try {
+            String sql = """
+                select client_ID as "Client ID", client_name as "Name",client_username as "Username",
+                client_email as "Email", client_phone as "Phone Number"
+                FROM clients order by client_id ASC;
+                    """;
+
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                int clientID = rs.getInt("Client ID");
+                String name = rs.getString("Name");
+                String phone = rs.getString("Phone");
+                String username = rs.getString("Username");
+                String email = rs.getString("Email");
+                Client c = new Client(clientID, name, phone,username, email);
+                clientList.add(c);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return clientList;
+    }
+
+    public static int addClient(Client c) {
+        int addConfirm = -1;
+        try {
+            String sql = """
+                    INSERT INTO clients (Client_ID, Client_Name, Client_Username, Client_Email,\s
+                    Client_Phone, Create_Date, Created_By, Last_Update, Last_Updated_By)\s
+                    VALUES
+                    (NULL, ?, ?, ?,	?,	?,	?, ?, ?);
+                    
+                    """;
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setString(1,c.getClientName());
+            ps.setString(2, c.getClientPhone());
+            ps.setString(3,c.getClientUsername());
+            ps.setString(4, c.getClientEmail());
+            ps.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now()));
+            ps.setString(6, Model.AuthorizedUser.getAuthorizedName());
+            ps.setTimestamp(7, Timestamp.valueOf(LocalDateTime.now()));
+            ps.setString(8,Model.AuthorizedUser.getAuthorizedName());
+            addConfirm = ps.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return addConfirm;
+    }
+
+    public static int editClient(Client c) {
+        int updateConfirm =-1;
+        try {
+            String sql = """
+                    UPDATE clients SET
+                    
+                    Client_Name = ?,
+                    Client_Username = ?,
+                    Client_Email = ?,
+                    Client_Phone = ?,
+                    Last_Update = ?,
+                    Last_Updated_By = ?
+                    
+                    WHERE Client_ID = ?;
+                                        
+                    """;
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setString(1, c.getClientName());
+            ps.setString(2, c.getClientUsername());
+            ps.setString(3,c.getClientEmail());
+            ps.setString(4,c.getClientPhone());
+            ps.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now()));
+            ps.setString(6,Model.AuthorizedUser.getAuthorizedName());
+            ps.setInt(7,c.getClientID());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return updateConfirm;
+    }
+
+    public static int deleteClient(Client c) {
+        int deleteConfirm = -1;
+        try {
+            String sql = "delete from clients where client_id = ?";
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setInt(1,c.getClientID());
+            deleteConfirm = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return deleteConfirm;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/DAO/DBCounselor.java
@@ -0,0 +1,108 @@
+package DAO;
+
+import Model.Counselor;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class DBCounselor {
+
+    public static ObservableList<Counselor> getCounselors() {
+        ObservableList<Counselor> counselorList = FXCollections.observableArrayList();
+        try {
+            String sql = """
+                    SELECT counselor_ID as "Counselor ID",
+                    counselor_name as "Name",
+                    counselor_username as "Username",
+                    counselor_email as "Email",
+                    counselor_phone as "Phone Number"
+                    FROM counselors order by counselor_id asc;
+                    """;
+
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                int counselorID = rs.getInt("counselor ID");
+                String name = rs.getString("Name");
+                String phone = rs.getString("Phone");
+                String username = rs.getString("Username");
+                String email = rs.getString("Email");
+                Counselor c = new Counselor(counselorID, name, phone,username, email);
+                counselorList.add(c);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return counselorList;
+    }
+
+    public static int addCounselor(Counselor c) {
+        int addConfirm = -1;
+        try {
+            String sql = """                
+                    INSERT INTO counselors (counselor_ID,
+                    counselor_Name, counselor_Username, counselor_Email,
+                    counselor_Phone, Create_Date, Created_By, Last_Update,
+                    Last_Updated_By)
+                    VALUES
+                    (NULL,?,?,?,?,?,?,?,?);
+                    """;
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setString(1,c.getCounselorName());
+            ps.setString(2, c.getCounselorPhone());
+            ps.setString(3,c.getCounselorUsername());
+            ps.setString(4, c.getCounselorEmail());
+            ps.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now()));
+            ps.setString(6, Model.AuthorizedUser.getAuthorizedName());
+            ps.setTimestamp(7, Timestamp.valueOf(LocalDateTime.now()));
+            ps.setString(8,Model.AuthorizedUser.getAuthorizedName());
+            addConfirm = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            }
+        return addConfirm;
+    }
+
+    public static int editCounselor(Counselor c) {
+        int updateConfirm =-1;
+        try {
+            String sql = """
+                    UPDATE counselors SET
+                    counselor_Name = ?,
+                    counselor_Username = ?,
+                    counselor_Email = ?,
+                    counselor_Phone = ?,
+                    Last_Update = ?,
+                    Last_Updated_By = ?
+                    WHERE counselor_ID = ?;
+                    """;
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setString(1, c.getCounselorName());
+            ps.setString(2, c.getCounselorUsername());
+            ps.setString(3,c.getCounselorEmail());
+            ps.setString(4,c.getCounselorPhone());
+            ps.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now()));
+            ps.setString(6,Model.AuthorizedUser.getAuthorizedName());
+            ps.setInt(7,c.getCounselorID());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+        }    return updateConfirm;
+    }
+
+    public static int deleteCounselor(Counselor c) {
+        int deleteConfirm = -1;
+        try {
+            String sql = "delete from counselors where counselor_id = ?";
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setInt(1,c.getCounselorID());
+            deleteConfirm = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return deleteConfirm;}
+}
